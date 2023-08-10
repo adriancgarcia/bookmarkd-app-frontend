b757aeb1e11bdbc4531dc886e7e74eafc0eec618
mdelvecchio <mdelvecchio3119@gmail.com>
2023-08-09 20:03:27 -0400

    adding the update action

--- src/actions.js
+++ src/actions.js
@@ -26,3 +26,27 @@ export const createAction = async ({request}) => {
     // redirect the user to the frontend index route
     return redirect('/')
 }
+
+export const updateAction = async ({request, params}) => {
+    // grabbing the id from the params
+    const id = params.id
+    // grabbing the data from the form
+    const formData = await request.formData()
+    // bulding out the updated bookmark
+    const updatedBookmark = {
+        title: formData.get("title"),
+        url: formData.get("url")
+    }
+    // sending the updated bookmark to our backend api
+    await fetch(`${baseUrl}/bookmarks/${id}`, {
+        // telling fetch to make a put request
+        method: 'Put',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // sending the json string of the updatedBookmark object
+        body: JSON.stringify(updatedBookmark)
+    })
+    // redirect the user back to the show page frontend route
+    return redirect(`/${id}`)
+}
