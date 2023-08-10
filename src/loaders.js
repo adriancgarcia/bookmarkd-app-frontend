dc76c41a47dfd25ac325be7c7348a929f515a4a6
Jonathan1295-09 <jcasasola955@gmail.com>
2023-08-09 19:14:11 -0500

    changes

--- src/loaders.js
+++ src/loaders.js
@@ -1,14 +1,14 @@
 import {baseUrl} from "./base_url"
 
-export const bookmarkloader = async () => {
+export const bookmarksLoader = async () => {
     const response = await fetch(`${baseUrl}/bookmarks`)
-    const bookmarks = await response .json()
+    const bookmarks = await response.json()
     return bookmarks
 };
 
-export const bookmarksloader = async ({params}) => {
+export const bookmarkLoader = async ({params}) => {
     const id = params.id
     const response = await fetch(`${baseUrl}/bookmarks${id}`)
-    const bookmarks = await response.json()
-    return bookmarks
+    const bookmark = await response.json()
+    return bookmark
 }
